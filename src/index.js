e96ca7871ab4b2ca7dcfb6e27d78f5e142ad5332
Tejaswini515 <tejaswinikosunam@gmail.com>
2018-09-04 15:29:23 -0500

    redux and react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Containers/App';
+import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore, compose} from 'redux';
+import reducer from './Store/reducer';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const store = createStore(reducer, composeEnhancers());
+
+ReactDOM.render(<Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+                </Provider>, document.getElementById('root'));
 registerServiceWorker();
